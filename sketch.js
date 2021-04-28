5c1e2950bd341a2bfc4ed47abf784049b35b859d
Amandajg13 <80363933+Amandajg13@users.noreply.github.com>
2021-04-28 15:24:22 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <script src="p5.js"></script>
+  <script src="p5.sound.js"></script>
+  <script src="sketch.js"></script>
+    <div class="content">
+    <style>
+<link rel="preconnect" href="https://fonts.gstatic.com"> 
+<link href="https://fonts.googleapis.com/css2?family=Antonio:wght@100&display=swap" rel="stylesheet">
+    </style>
+    <title align="center">Sound</title>
+    <link rel="stylesheet" type="text/css" href="index.css">
+  </head>
+  <body>
+      <h1 align="center">Hello there,</h1>
+     
+      <p align="center">Welcome to my Github site.</p>
+    <p><a href="about.html" align="center">Click here to visit my about page!</a></p>
+    <p><a href="index.html" align="center">Click here to visit my home page!</a></p>
+  
+    </div>
+  </body>
+</html>
